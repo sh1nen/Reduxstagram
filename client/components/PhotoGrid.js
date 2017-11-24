4be4c3b1f030710975f0c8bdf81d4263b9fb17a7
stosik <tosikszymon@gmail.com>
2017-11-24 13:10:39 +0100

    Added store

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PhotoGrid = React.createClass({
+    render() {
+        return (
+            <div className="photo-grid">
+                <p>I'm the photo grid</p>
+            </div>
+        )
+    }
+});
+
+export default PhotoGrid;
